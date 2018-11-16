6c98e4adf4b319ea70846f839626edaaacf67e8d
moonrailgun <moonrailgun@gmail.com>
2018-11-16 10:03:33 +0800

    :star: kanban逻辑

---
+++ imports/api/kanban/server/publications.js
@@ -0,0 +1,26 @@
+import { Meteor } from 'meteor/meteor';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { Kanban } from '../kanban.js';
+
+Meteor.publish('kanban.all', function() {
+  const { userId } = this;
+
+  return {
+    find() {
+      if(!userId) {
+        return this.ready();
+      }
+
+      const query = {
+        $or: [
+          { userId },
+          {
+            members: userId
+          }
+        ]
+      }
+
+      return Kanban.find(query)
+    }
+  }
+});
--- imports/ui/routers/Kanban.jsx
+++ imports/ui/routers/Kanban.jsx
@@ -4,17 +4,22 @@ import { Kanban } from '/imports/api/kanban/kanban';
 
 class KanbanContainer extends React.Component {
   render() {
+    const {
+      params
+    } = this.props.match;
+    const { kanbanId } = params;
     return (
       <div>
         当前看板信息:
-        {JSON.stringify(this.props.kanban)}
+        {JSON.stringify(this.props.allKanban)}
       </div>
     )
   }
 }
 
-export default withTracker(() => {
+export default withTracker(({match}) => {
+  const kanbanId = match.params.kanbanId;
   return {
-    kanban: Kanban.findOne(),
+    allKanban: Meteor.subscribe('kanban.all'),
   }
 })(KanbanContainer);
