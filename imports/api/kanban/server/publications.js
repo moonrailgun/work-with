ad66c4cb5c3292a05b5eb4069fedfbc0a3b927ce
moonrailgun <moonrailgun@gmail.com>
2018-11-27 18:07:14 +0800

    :star: 看板列表信息

--- imports/api/kanban/server/publications.js
+++ imports/api/kanban/server/publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Kanban } from '../kanban.js';
+import { KanbanColumn } from '../kanbanColumn.js';
 
 Meteor.publish('kanban.all', function() {
   const { userId } = this;
@@ -19,4 +20,19 @@ Meteor.publish('kanban.all', function() {
   }
 
   return Kanban.find(query)
+});
+
+// TODO: 可能需要处理一些权限的问题
+Meteor.publish('kanban.cols.all', function({ kanbanId }) {
+  const { userId } = this;
+
+  if(!userId) {
+    return this.ready();
+  }
+
+  const query = {
+    kanbanId,
+  }
+
+  return KanbanColumn.find(query);
 });
--- imports/ui/routers/Kanban.jsx
+++ imports/ui/routers/Kanban.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Kanban } from '/imports/api/kanban/kanban';
+import { KanbanColumn } from '/imports/api/kanban/kanbanColumn';
 import KanbanContainer from '/imports/ui/components/KanbanContainer';
 
 import { withStyles } from '@material-ui/core/styles';
@@ -29,6 +30,7 @@ class KanbanRoute extends React.Component {
       <div className={classes.root}>
         当前看板信息:
         {JSON.stringify(this.props.kanbanInfo)}
+        {JSON.stringify(this.props.kanbanCols)}
         <div className={classes.kanbanContainer}>
           <KanbanContainer kanbanId={kanbanId} />
         </div>
@@ -37,14 +39,22 @@ class KanbanRoute extends React.Component {
   }
 }
 
-console.log('kanban', Kanban);
-
 const route = withTracker(({match}) => {
   const kanbanId = match.params.kanbanId;
-  const allKanbanHandler = Meteor.subscribe('kanban.all')
+  const allKanbanHandler = Meteor.subscribe('kanban.all');
+  const allKanbanColsHandler = Meteor.subscribe('kanban.cols.all', { kanbanId });
+  const kanbanInfo = Kanban.find(kanbanId).fetch()[0];
+  let kanbanCols = [];
+  if(kanbanInfo && kanbanInfo.cols) {
+    // 这里有个排序
+    kanbanCols = KanbanColumn.find({
+      $or: kanbanInfo.cols.map(_id => ({_id}))
+    }).fetch();
+  }
   return {
-    loading: !allKanbanHandler.ready(),
-    kanbanInfo: Kanban.find(kanbanId).fetch()[0],
+    loading: !allKanbanHandler.ready() || !allKanbanColsHandler.ready(),
+    kanbanInfo,
+    kanbanCols,
   }
 })(KanbanRoute)
 
