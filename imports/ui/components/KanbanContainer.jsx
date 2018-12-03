78641fcd1509c7a9d74f458d668a1ac3d6aaba80
moonrailgun <moonrailgun@gmail.com>
2018-12-03 14:22:23 +0800

    :star: 卡片移动

--- imports/ui/components/KanbanContainer.jsx
+++ imports/ui/components/KanbanContainer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { DragDropContext } from 'react-beautiful-dnd';
 import classNames from 'classnames';
 import KanbanColumn from './KanbanColumn';
-import { addKanbanColumn } from '/imports/api/kanban/methods';
+import { addKanbanColumn, moveCard } from '/imports/api/kanban/methods';
 
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -19,12 +19,32 @@ const styles = ({shape, spacing, palette, shadows}) => ({
 
 class KanbanContainer extends React.Component {
   _handleDropEnd(result) {
-    const { source, destination } = result;
+    const { source, destination, draggableId: cardId } = result;
     if(!destination) {
       return;
     }
 
-    console.log('source, destination', source, destination);
+    const {
+      droppableId: fromColId,
+      index: fromIndex,
+    } = source;
+    const {
+      droppableId: toColId,
+      index: toIndex,
+    } = destination;
+
+    const pkg = {
+      cardId,
+      fromColId,
+      fromIndex,
+      toColId,
+      toIndex,
+    };
+
+    console.log('move card', pkg);
+    moveCard.call(pkg, err => console.log(err));
+
+    return true;
   }
 
   _handleAddCol(title) {
