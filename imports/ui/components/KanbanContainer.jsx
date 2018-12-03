1cb202f68a54e78a400e42d38f2693f6dcf565f0
moonrailgun <moonrailgun@gmail.com>
2018-12-03 13:59:54 +0800

    :hammer: 删除部分无用的示例代码

--- imports/ui/components/KanbanContainer.jsx
+++ imports/ui/components/KanbanContainer.jsx
@@ -18,45 +18,6 @@ const styles = ({shape, spacing, palette, shadows}) => ({
 })
 
 class KanbanContainer extends React.Component {
-  state = {
-    cols: [{
-      _id: '1',
-      title: 'TODO',
-      cards: [{
-        _id: "123",
-        content: 'test11231'
-      }]
-    }, {
-      _id: '122',
-      title: '处理中',
-      cards: [{
-        _id: "12314",
-        content: '1eede'
-      }]
-    }, {
-      _id: '1221dd',
-      title: '处理中1',
-      cards: [{
-        _id: "123149",
-        content: '1eeacde'
-      }]
-    }, {
-      _id: '12222',
-      title: '处理中2',
-      cards: [{
-        _id: "12314qq",
-        content: '1ewdede'
-      }]
-    }, {
-      _id: '22',
-      title: 'DONE',
-      cards: [{
-        _id: "1234",
-        content: 'uudd'
-      }]
-    }]
-  }
-
   _handleDropEnd(result) {
     const { source, destination } = result;
     if(!destination) {
