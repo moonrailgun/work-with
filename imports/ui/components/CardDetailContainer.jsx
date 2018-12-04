584363b2da769f7debbb75ca59cc15f15dee8c70
moonrailgun <moonrailgun@gmail.com>
2018-12-04 18:02:15 +0800

    :star: 侧边栏切换按钮

--- imports/ui/components/CardDetailContainer.jsx
+++ imports/ui/components/CardDetailContainer.jsx
@@ -1,26 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Button from '@material-ui/core/Button';
-import FirstPageIcon from '@material-ui/icon/FirstPage';
-import LastPageIcon from '@material-ui/icon/LastPage';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import LastPageIcon from '@material-ui/icons/LastPage';
 
 const Container = styled.div`
   display: flex;
   height: 100%;
+  background-color: white;
 
   nav {
-    background-color: white;
-    width: 40px;
+    width: 2.5rem;
+    position: relative;
+    padding: 4rem 0;
+
+    &::after {
+      content: ' ';
+      width: 1px;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      background-color: #ccc;
+    }
+
+    button {
+      display: block;
+      margin: auto;
+      padding: 0;
+      width: 2rem;
+      height: 2rem;
+      border-radius: 3px;
+      border: 0;
+      cursor: pointer;
+
+      &:hover {
+        background-color: rgba(0, 0, 0, 0.2);
+      }
+    }
   }
 `
 
 class CardDetailContainer extends React.Component {
+  state = {
+    isShow: true,
+  }
+
   render() {
     return (
       <Container>
         <nav>
-          <Button><LastPageIcon /></Button>
+          <button onClick={() => this.setState({isShow: !this.state.isShow})}>
+            <LastPageIcon />
+          </button>
         </nav>
         card detail....
       </Container>
