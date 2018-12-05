d96a984a89bd75c25837c074ba68c971b9d71812
moonrailgun <moonrailgun@gmail.com>
2018-12-05 16:34:02 +0800

    :star: 卡片项 更多操作列表

--- imports/ui/components/CardItem.jsx
+++ imports/ui/components/CardItem.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import Markdown from './Markdown';
 
+import Popper from '@material-ui/core/Popper';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
 import NotesIcon from '@material-ui/icons/Notes';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
@@ -38,12 +44,53 @@ const Root = styled.div`
 
   .action {
     cursor: pointer;
+
+    & > svg {
+      &:hover, &:active {
+        color: ${props => props.theme.palette.primary['light']};
+      }
+    }
   }
 `
 
 class CardItem extends React.Component {
-  _handleClickMore() {
-    console.log('TODO: 待实现');
+  state = {
+    isShowToggleMenu: false
+  }
+  _toggleMenuRef = null;
+
+  _handleClickMore(e) {
+    this._toggleMenuRef = e.currentTarget;
+    this.setState({isShowToggleMenu: true})
+  }
+
+  renderToggleMenu() {
+    return (
+      <Popper
+        open={this.state.isShowToggleMenu}
+        placement="bottom-end"
+        anchorEl={this._toggleMenuRef}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{ transformOrigin: placement === 'bottom-end' ? 'right top' : 'right bottom' }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={() => this.setState({isShowToggleMenu: false})}>
+                <MenuList>
+                  <MenuItem onClick={() => console.log('aaaaa')}>Profile</MenuItem>
+                  <MenuItem onClick={() => console.log('aaaaa')}>My account</MenuItem>
+                  <MenuItem onClick={() => console.log('aaaaa')}>Logout</MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    )
   }
 
   render() {
@@ -53,7 +100,10 @@ class CardItem extends React.Component {
         <div className="main">
           <Markdown>{this.props.children}</Markdown>
         </div>
-        <div className="action" onClick={() => this._handleClickMore()}><MoreHorizIcon /></div>
+        <div className="action" onClick={(e) => this._handleClickMore(e)}>
+          <MoreHorizIcon />
+        </div>
+        { this.renderToggleMenu() }
       </Root>
     )
   }
