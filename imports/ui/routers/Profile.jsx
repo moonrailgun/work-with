00b4396ade24edf8ff3c22b654bd298b35b5f0db
moonrailgun <moonrailgun@gmail.com>
2018-12-26 17:33:15 +0800

    :star: 详情页基本UI框架

--- imports/ui/routers/Profile.jsx
+++ imports/ui/routers/Profile.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
+import styled from 'styled-components';
+
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const Container = styled(Grid)`
+  padding-top: ${props => props.theme.spacing.unit * 2}px;
+`
+
+const ProfileCard = styled(Paper)`
+  padding: ${props => props.theme.spacing.unit}px;
+  margin-bottom: ${props => props.theme.spacing.unit}px;
+`
 
 class ProfileRoute extends React.Component {
   render() {
@@ -10,10 +23,25 @@ class ProfileRoute extends React.Component {
     } = this.props;
 
     return (
-      <div>
-        <span>允许编辑: {JSON.stringify(allowEdit)}</span>
-        {JSON.stringify(userInfo)}
-      </div>
+      <Container
+        container
+        direction="row"
+        justify="center"
+      >
+        <Grid container item sm={10} spacing={16}>
+          <Grid item sm={8}>
+            <ProfileCard>用户信息</ProfileCard>
+          </Grid>
+          <Grid item sm={4}>
+            <ProfileCard>
+              <div>
+                <span>允许编辑: {JSON.stringify(allowEdit)}</span>
+                {JSON.stringify(userInfo)}
+              </div>
+            </ProfileCard>
+          </Grid>
+        </Grid>
+      </Container>
     )
   }
 }
