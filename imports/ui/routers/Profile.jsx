184884e1ff43f9046fab2b48606fde11ea6ac169
moonrailgun <moonrailgun@gmail.com>
2018-12-27 14:31:44 +0800

    :star: 文件上传

--- imports/ui/routers/Profile.jsx
+++ imports/ui/routers/Profile.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import styled from 'styled-components';
+import { Avatar } from '/imports/api/files/avatar';
 
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
 const Container = styled(Grid)`
   padding-top: ${props => props.theme.spacing.unit * 2}px;
@@ -16,6 +18,34 @@ const ProfileCard = styled(Paper)`
 `
 
 class ProfileRoute extends React.Component {
+  handleShowUpload = () => {
+    this.refs.avatarFile && this.refs.avatarFile.click();
+  }
+
+  handleUpload = (e) => {
+    let files = e.currentTarget.files;
+    if(files && files[0]) {
+      const file = files[0];
+      Avatar.insert({
+        file,
+        streams: 'dynamic',
+        chunkSize: 'dynamic',
+        onStart(err, fileData) {
+          console.log('开始上传');
+        },
+        onProgress(progress) {
+          console.log('上传进度', progress);
+        },
+        onError(err, fileData) {
+          console.log('上传失败');
+        },
+        onUploaded(err, fileRef) {
+          console.log('上传完毕');
+        }
+      })
+    }
+  }
+
   render() {
     const {
       allowEdit,
@@ -30,7 +60,19 @@ class ProfileRoute extends React.Component {
       >
         <Grid container item sm={10} spacing={16}>
           <Grid item sm={8}>
-            <ProfileCard>用户信息</ProfileCard>
+            <ProfileCard>
+              用户信息
+              {
+                allowEdit && (
+                  <Fragment>
+                    <Button variant="outlined" color="primary" onClick={this.handleShowUpload}>
+                      上传头像
+                    </Button>
+                    <input type="file" style={{display: 'none'}} ref="avatarFile" onChange={this.handleUpload} />
+                  </Fragment>
+                )
+              }
+            </ProfileCard>
           </Grid>
           <Grid item sm={4}>
             <ProfileCard>
