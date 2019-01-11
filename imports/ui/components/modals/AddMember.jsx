b81a992d24d0fed0aa37c9384f36279542ec5736
moonrailgun <moonrailgun@gmail.com>
2019-01-11 17:33:36 +0800

    :star: 增加react-select并添加建议

--- imports/ui/components/modals/AddMember.jsx
+++ imports/ui/components/modals/AddMember.jsx
@@ -1,5 +1,6 @@
 // 添加看板成员modal
 import React from 'react';
+import Select from 'react-select';
 import styled from 'styled-components';
 import { withTracker } from 'meteor/react-meteor-data';
 
@@ -7,9 +8,17 @@ class AddMember extends React.Component {
   render() {
     console.log('this.props.users', this.props.users);
 
+    const suggestions = this.props.users.map(user => {
+      return {
+        label: user.emails.address,
+        value: user._id
+      }
+    })
+
     return (
       <div>
         123, loading: {JSON.stringify(this.props.loading)}
+        <Select options={suggestions} isClearable />
       </div>
     )
   }
